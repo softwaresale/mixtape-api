5b878a600db48324b8c393690d63cd2104d3e052
Charlie Sale <softwaresale01@gmail.com>
2023-09-13 14:03:09 -0400

    Initial commit

---
+++ src/main/java/com/mixtape/mixtapeapi/MixtapeApiApplication.java
@@ -0,0 +1,13 @@
+package com.mixtape.mixtapeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixtapeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixtapeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mixtape/mixtapeapi/MixtapeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixtape.mixtapeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixtapeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
