662f4bbf2f91a7c1f69eba67052e99c2f27c2345
alexfrey317 <alexfrey317@gmail.com>
2023-09-20 17:18:52 -0400

    Created Skeleton code of searching (Controller and Service)

---
+++ src/main/java/com/mixtape/mixtapeapi/search/SearchService.java
@@ -0,0 +1,16 @@
+package com.mixtape.mixtapeapi.search;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SearchService {
+    public SearchService() {}
+
+    public List<String> getUsersByName(String name) {
+
+        return null;
+    }
+
+}
