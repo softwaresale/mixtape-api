49bb8b34f7a129f35db48b9d9f593d07fca9a7e3
alexfrey317 <alexfrey317@gmail.com>
2023-09-20 17:02:32 -0400

    Added put request to mixtape controller

--- src/main/java/com/mixtape/mixtapeapi/mixtape/MixtapeController.java
+++ src/main/java/com/mixtape/mixtapeapi/mixtape/MixtapeController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,4 +33,10 @@ public class MixtapeController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
     }
+
+    @PutMapping("/{id}")
+    public Mixtape update(@RequestBody Mixtape mixtape) {
+        return mixtapeService.save(mixtape);
+    }
+
 }
