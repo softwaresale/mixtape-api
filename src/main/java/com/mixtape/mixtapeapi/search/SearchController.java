560a90d0ec7b200d162b60bd5e228eb1ea01f369
alexfrey317 <alexfrey317@gmail.com>
2023-09-20 17:18:52 -0400

    Created Skeleton code of searching (Controller and Service)

---
+++ src/main/java/com/mixtape/mixtapeapi/search/SearchController.java
@@ -0,0 +1,23 @@
+package com.mixtape.mixtapeapi.search;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/search")
+public class SearchController {
+    private final SearchService searchService;
+
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @GetMapping
+    public List<String> getUsers(@RequestParam String userName) {
+        return searchService.getUsersByName(userName);
+    }
+}
