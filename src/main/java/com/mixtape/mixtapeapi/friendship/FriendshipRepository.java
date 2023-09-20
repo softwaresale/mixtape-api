3829056ce290ec4b8d97fc4084c4e058a055949d
alexfrey317 <alexfrey317@gmail.com>
2023-09-20 17:11:56 -0400

    Created Friendship controller, repository, and service for getting and creating

---
+++ src/main/java/com/mixtape/mixtapeapi/friendship/FriendshipRepository.java
@@ -0,0 +1,6 @@
+package com.mixtape.mixtapeapi.friendship;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendshipRepository extends JpaRepository<Friendship, String> {
+}
