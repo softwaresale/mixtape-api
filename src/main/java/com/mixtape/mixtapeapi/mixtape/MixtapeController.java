b0b727102e373c8c798883ab628faf1d8855771a
alexfrey317 <alexfrey317@gmail.com>
2023-09-20 16:19:17 -0400

    Created Base Controller, Repository, and Service for mixtape

---
+++ src/main/java/com/mixtape/mixtapeapi/mixtape/MixtapeController.java
@@ -0,0 +1,35 @@
+package com.mixtape.mixtapeapi.mixtape;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/v1/mixtape")
+public class MixtapeController {
+    private final MixtapeService mixtapeService;
+
+    public MixtapeController(MixtapeService mixtapeService) {
+        this.mixtapeService = mixtapeService;
+    }
+
+    @PostMapping
+    public Mixtape createNew(@RequestBody Mixtape newMixtape) {
+        return mixtapeService.save(newMixtape);
+    }
+
+    @GetMapping("/{id}")
+    public Mixtape getById(@PathVariable String id) {
+        try {
+            return mixtapeService.findMixtape(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+}
