ab9b9783e337416d13f378e9deb625565e88d94f
alexfrey317 <alexfrey317@gmail.com>
2023-09-20 20:32:37 -0400

    Fixed Client Credentials Spotify Api

--- src/main/java/com/mixtape/mixtapeapi/search/SearchService.java
+++ src/main/java/com/mixtape/mixtapeapi/search/SearchService.java
@@ -1,15 +1,20 @@
 package com.mixtape.mixtapeapi.search;
 
 import org.springframework.stereotype.Service;
+import se.michaelthelin.spotify.SpotifyApi;
 
 import java.util.List;
 
 @Service
 public class SearchService {
-    public SearchService() {}
+    private final SpotifyApi spotifyApi;
+
+    public SearchService(SpotifyApi spotifyApi) {
+        this.spotifyApi = spotifyApi;
+    }
 
     public List<String> getUsersByName(String name) {
-
+        // TODO: Not sure if feasible
         return null;
     }
 
