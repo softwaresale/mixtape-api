449c486355a8179656757e68d8b05d5c7b27fd1d
alexfrey317 <alexfrey317@gmail.com>
2023-09-27 15:37:40 -0400

    Updated invitation stuff to isolate playlist service

--- src/main/java/com/mixtape/mixtapeapi/invitation/InvitationController.java
+++ src/main/java/com/mixtape/mixtapeapi/invitation/InvitationController.java
@@ -1,12 +1,10 @@
 package com.mixtape.mixtapeapi.invitation;
 
 import com.mixtape.mixtapeapi.playlist.Playlist;
-import com.mixtape.mixtapeapi.playlist.PlaylistService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -15,11 +13,9 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/api/v1/invitation")
 public class InvitationController {
     private final InvitationService invitationService;
-    private final PlaylistService playlistService;
 
-    public InvitationController(InvitationService invitationService, PlaylistService playlistService) {
+    public InvitationController(InvitationService invitationService) {
         this.invitationService = invitationService;
-        this.playlistService = playlistService;
     }
 
     @GetMapping("/{id}")
@@ -33,8 +29,9 @@ public class InvitationController {
         return invitationService.save(newInvitation);
     }
 
-    @PostMapping("/playlist")
-    public Playlist createNewPlaylist(@RequestBody Invitation invitation) {
-        return playlistService.createPlaylistFromInvitation(invitation);
+    @PostMapping("/{id}/playlist")
+    public Playlist createNewPlaylist(@PathVariable String id) {
+        return invitationService.createPlaylistFromInvitationId(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
     }
 }
--- src/main/java/com/mixtape/mixtapeapi/invitation/InvitationService.java
+++ src/main/java/com/mixtape/mixtapeapi/invitation/InvitationService.java
@@ -1,5 +1,7 @@
 package com.mixtape.mixtapeapi.invitation;
 
+import com.mixtape.mixtapeapi.playlist.Playlist;
+import com.mixtape.mixtapeapi.playlist.PlaylistService;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -7,9 +9,11 @@ import java.util.Optional;
 @Service
 public class InvitationService {
     private final InvitationRepository invitationRepository;
+    private final PlaylistService playlistService;
 
-    public InvitationService(InvitationRepository invitationRepository) {
+    public InvitationService(InvitationRepository invitationRepository, PlaylistService playlistService) {
         this.invitationRepository = invitationRepository;
+        this.playlistService = playlistService;
     }
 
     public Optional<Invitation> findInvitation(String id) {
@@ -18,5 +22,16 @@ public class InvitationService {
 
     public Invitation save(Invitation newInvitation) {
         return invitationRepository.save(newInvitation);
+    }
+
+    public Optional<Playlist> createPlaylistFromInvitationId(String id) {
+        // Grab invitation
+        Optional<Invitation> invitation = findInvitation(id);
+
+        // Check if exists
+        if (invitation.isEmpty()) return Optional.empty();
+
+        // Call playlist service to complete work
+        return Optional.of(playlistService.createPlaylistFromInvitation(invitation.get()));
     }
 }
