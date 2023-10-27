0c24baec64d0eee6b1942c8e5788adc459bc6a69
Charlie Sale <softwaresale01@gmail.com>
2023-10-26 22:23:26 -0400

    Simplify profile controller with AbstractRestController
    Combine /me and /{id} endpoints with abstract rest controller

--- src/main/java/com/mixtape/mixtapeapi/profile/ProfileController.java
+++ src/main/java/com/mixtape/mixtapeapi/profile/ProfileController.java
@@ -1,27 +1,19 @@
 package com.mixtape.mixtapeapi.profile;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import com.mixtape.mixtapeapi.AbstractRestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/v1/profile")
-public class ProfileController {
-    private final ProfileService profileService;
+public class ProfileController extends AbstractRestController {
 
     public ProfileController(ProfileService profileService) {
-        this.profileService = profileService;
+        super(profileService);
     }
 
     @PostMapping
@@ -29,17 +21,8 @@ public class ProfileController {
         return profileService.save(newProfile);
     }
 
-    @GetMapping("/me")
-    public Profile getByCurrentUser(Principal currentPrincipal) {
-        var userAuthentication = (JwtAuthenticationToken) currentPrincipal;
-        String id = userAuthentication.getName();
-        return profileService.findProfile(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Profile could not be found"));
-    }
-
     @GetMapping("/{id}")
     public Profile getById(@PathVariable String id) {
-        return profileService.findProfile(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return resolveProfileOr404(id);
     }
 }
