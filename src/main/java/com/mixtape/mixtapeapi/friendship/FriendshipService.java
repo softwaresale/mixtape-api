9f3ce8b240a577f7a02e02ede56d52238557d350
alexfrey317 <70394182+alexfrey317@users.noreply.github.com>
2023-10-30 17:35:51 -0400

    5,6,10 deleting tapes playlists and friends (#9)
    * Implemented Deletion of mixtapes
    
    * Implemented Deletion of playlists
    
    * Implemented Deletion of playlists fully
    
    * Implemented Deletion of friendships

--- src/main/java/com/mixtape/mixtapeapi/friendship/FriendshipService.java
+++ src/main/java/com/mixtape/mixtapeapi/friendship/FriendshipService.java
@@ -1,9 +1,14 @@
 package com.mixtape.mixtapeapi.friendship;
 
 import com.mixtape.mixtapeapi.invitation.Invitation;
+import com.mixtape.mixtapeapi.playlist.Playlist;
+import com.mixtape.mixtapeapi.playlist.PlaylistService;
 import com.mixtape.mixtapeapi.profile.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -11,9 +16,11 @@ import java.util.stream.Collectors;
 @Service
 public class FriendshipService {
     private final FriendshipRepository friendshipRepository;
+    private final PlaylistService playlistService;
 
-    public FriendshipService(FriendshipRepository friendshipRepository) {
+    public FriendshipService(FriendshipRepository friendshipRepository, PlaylistService playlistService) {
         this.friendshipRepository = friendshipRepository;
+        this.playlistService = playlistService;
     }
 
     public Optional<Friendship> findFriendship(String id) {
@@ -37,5 +44,18 @@ public class FriendshipService {
                     }
                 })
                 .collect(Collectors.toList());
+    }
+
+    public void deleteFriendship(String friendshipId) throws IOException {
+        // Grab friendship
+        Friendship friendship = findFriendship(friendshipId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Friendship does not exist"));
+
+        // Delete playlists between them
+        playlistService.findPlaylistsForProfile(friendship.getInitiator())
+                .stream()
+                .filter(playlist -> playlist.getTarget().equals(friendship.getTarget()))
+                .map(Playlist::getId)
+                .forEach(playlistService::deleteById);
     }
 }
