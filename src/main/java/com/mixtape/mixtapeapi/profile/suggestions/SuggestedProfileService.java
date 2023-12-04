fb9d8fe6c96aa611b26f012dfa9c45b04db207cf
Charlie Sale <softwaresale01@gmail.com>
2023-12-04 15:11:58 -0500

    Don't make suggestions for people you're friends with or have blocked

--- src/main/java/com/mixtape/mixtapeapi/profile/suggestions/SuggestedProfileService.java
+++ src/main/java/com/mixtape/mixtapeapi/profile/suggestions/SuggestedProfileService.java
@@ -1,7 +1,9 @@
 package com.mixtape.mixtapeapi.profile.suggestions;
 
+import com.mixtape.mixtapeapi.friendship.FriendshipService;
 import com.mixtape.mixtapeapi.profile.Profile;
 import com.mixtape.mixtapeapi.profile.ProfileService;
+import com.mixtape.mixtapeapi.profile.blocking.BlockedProfileService;
 import com.mixtape.mixtapeapi.spotify.SpotifyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,11 +21,15 @@ public class SuggestedProfileService {
     private static final Logger logger = LoggerFactory.getLogger(SuggestedProfileService.class);
 
     private final ProfileService profileService;
+    private final FriendshipService friendshipService;
+    private final BlockedProfileService blockedProfileService;
     private final SuggestedProfileRepository repository;
     private final SpotifyService spotifyService;
 
-    public SuggestedProfileService(ProfileService profileService, SuggestedProfileRepository repository, SpotifyService spotifyService) {
+    public SuggestedProfileService(ProfileService profileService, FriendshipService friendshipService, BlockedProfileService blockedProfileService, SuggestedProfileRepository repository, SpotifyService spotifyService) {
         this.profileService = profileService;
+        this.friendshipService = friendshipService;
+        this.blockedProfileService = blockedProfileService;
         this.repository = repository;
         this.spotifyService = spotifyService;
     }
@@ -50,8 +56,8 @@ public class SuggestedProfileService {
 
         logger.info("Computing suggested users for profile '{}' with provider token '{}'", newProfile, providerToken);
 
-        HashSet<String> excluded = new HashSet<>(excludeProfileIDs);
-        excluded.add(newProfile.getId());
+        // get a set of candidates to ignore
+        Set<String> excluded = getCandidateFilters(newProfile, excludeProfileIDs);
 
         List<String> spotifyIDs = profileService.getSpotifyIDsForAllUsersExcept(excluded);
         List<String> followedUsers = spotifyService.checkFollowsAnyUsers(providerToken, spotifyIDs);
@@ -79,5 +85,23 @@ public class SuggestedProfileService {
 
     public void deleteSuggestionsBetweenUsers(Profile profile1, Profile profile2) {
         this.repository.deleteAllBySuggestedAndSuggestionForOrSuggestionForAndSuggested(profile1, profile2, profile1, profile2);
+    }
+
+    private Set<String> getCandidateFilters(Profile newProfile, Set<String> ignoredUsers) {
+
+        Set<String> allIgnored = new HashSet<>(ignoredUsers);
+        allIgnored.add(newProfile.getId());
+
+        // don't suggest blocked profiles
+        this.blockedProfileService.getBlockedProfiles(newProfile).stream()
+                .map(Profile::getId)
+                .forEach(allIgnored::add);
+
+        // don't suggest people you're already friends with
+        this.friendshipService.findFriendsForProfile(newProfile).stream()
+                .map(Profile::getId)
+                .forEach(allIgnored::add);
+
+        return allIgnored;
     }
 }
