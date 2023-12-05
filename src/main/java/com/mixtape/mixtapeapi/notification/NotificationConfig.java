d3ede3155e7a5249f0f3b07ae3518d0ba990c5cb
Charlie Sale <softwaresale01@gmail.com>
2023-12-05 18:28:25 -0500

    Turn on push notifications in prod

--- src/main/java/com/mixtape/mixtapeapi/notification/NotificationConfig.java
+++ src/main/java/com/mixtape/mixtapeapi/notification/NotificationConfig.java
@@ -44,14 +44,14 @@ public class NotificationConfig {
         return new OneSignalDefaultApiFactory(defaultNotificationApi());
     }
 
-    @Profile("!prod")
+    @Profile("prod")
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public PushNotificationService oneSignalNotificationService() {
         return new OneSignalPushNotificationService(oneSignalDefaultApiFactory(), notificationsAppId());
     }
 
-    @Profile("prod")
+    @Profile("!prod")
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public PushNotificationService nopPushNotificationService() {
