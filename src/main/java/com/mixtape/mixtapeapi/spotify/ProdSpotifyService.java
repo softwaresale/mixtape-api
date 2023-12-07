0c5f73c64bf5442b3eb8210cf17ca8df4a84c707
alexfrey317 <alexfrey317@gmail.com>
2023-12-06 21:33:35 -0500

    Small changes to help rebase

--- src/main/java/com/mixtape/mixtapeapi/spotify/ProdSpotifyService.java
+++ src/main/java/com/mixtape/mixtapeapi/spotify/ProdSpotifyService.java
@@ -1,14 +1,17 @@
 package com.mixtape.mixtapeapi.spotify;
 
 import com.mixtape.mixtapeapi.tracks.TrackInfo;
+import com.mixtape.mixtapeapi.util.ListUtils;
 import org.apache.hc.core5.http.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.enums.ModelObjectType;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
+import se.michaelthelin.spotify.model_objects.miscellaneous.Device;
 import se.michaelthelin.spotify.model_objects.specification.ArtistSimplified;
 import se.michaelthelin.spotify.model_objects.specification.Image;
 import se.michaelthelin.spotify.model_objects.specification.PagingCursorbased;
@@ -19,10 +22,7 @@ import se.michaelthelin.spotify.model_objects.specification.TrackSimplified;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ProdSpotifyService implements SpotifyService {
@@ -77,6 +77,50 @@ public class ProdSpotifyService implements SpotifyService {
             logger.error("Failed to get mixtape duration", spotifyExe);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to perform spotify network request", spotifyExe);
         }
+    }
+
+    @Override
+    public List<String> checkFollowsAnyUsers(String userProviderToken, List<String> spotifyUserIDs) throws ResponseStatusException {
+
+        if (spotifyUserIDs.isEmpty()) {
+            return List.of();
+        }
+
+        // Save the token state
+        String savedAccessToken = this.spotifyApi.getAccessToken();
+        this.spotifyApi.setAccessToken(userProviderToken);
+
+        List<String> allFollowedUsers = new ArrayList<>();
+        List<List<String>> idPartitions = ListUtils.partition(spotifyUserIDs, 50);
+        for (List<String> part : idPartitions) {
+            List<String> checkedUsers = checkFollowsSet(part);
+            allFollowedUsers.addAll(checkedUsers);
+        }
+
+        this.spotifyApi.setAccessToken(savedAccessToken);
+
+        return allFollowedUsers;
+    }
+
+    private List<String> checkFollowsSet(List<String> userIDsSubset) {
+        Boolean[] results;
+        try {
+            results = spotifyApi.checkCurrentUserFollowsArtistsOrUsers(ModelObjectType.USER, userIDsSubset.toArray(String[]::new))
+                    .build()
+                    .execute();
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            logger.error("Failed to check if user follows other users", e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to check if user follows other users", e);
+        }
+
+        List<String> followedUsers = new ArrayList<>();
+        for (int i = 0; i < userIDsSubset.size(); i++) {
+            if (results[i]) {
+                followedUsers.add(userIDsSubset.get(i));
+            }
+        }
+
+        return followedUsers;
     }
 
     @Override
@@ -152,5 +196,77 @@ public class ProdSpotifyService implements SpotifyService {
 
     public Instant getTokenExpiresAt() {
         return tokenExpiresAt;
+    }
+
+    @Override
+    public void enqueueSongs(String token, List<String> ids) throws ResponseStatusException {
+        // Save the token state
+        String savedAccessToken = this.spotifyApi.getAccessToken();
+        this.spotifyApi.setAccessToken(token);
+
+        // figure out what device to enqueue to
+        // Optional<Device> toEnqueueDevice = getDeviceToEnqueueTo(getUserDevices(token));
+
+        for (String id : ids) {
+            String uri = formatSpotifyUriForTrack(id);
+            try {
+                var builder = this.spotifyApi.addItemToUsersPlaybackQueue(uri);
+                /*
+                toEnqueueDevice.ifPresent(dev -> {
+                    logger.info("Enqueuing mixtape to device {} (id={})", dev.getName(), dev.getId());
+                    builder.device_id(dev.getId());
+                });
+                 */
+
+                builder
+                        .build()
+                        .execute();
+            } catch (IOException | SpotifyWebApiException | ParseException e) {
+                logger.error("Error while enqueueing song for user", e);
+                this.spotifyApi.setAccessToken(savedAccessToken);
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to perform spotify request", e);
+            }
+        }
+
+        this.spotifyApi.setAccessToken(savedAccessToken);
+    }
+
+    private List<Device> getUserDevices(String token) throws ResponseStatusException {
+        String savedAccessToken = this.spotifyApi.getAccessToken();
+        this.spotifyApi.setAccessToken(token);
+
+        List<Device> devices;
+
+        try {
+            Device[] userDevices = this.spotifyApi.getUsersAvailableDevices()
+                    .build()
+                    .execute();
+
+            devices = List.of(userDevices);
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            logger.error("Error while fetching user devices", e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to perform spotify request", e);
+        }
+
+        this.spotifyApi.setAccessToken(savedAccessToken);
+        return devices;
+    }
+
+    private static Optional<Device> getDeviceToEnqueueTo(List<Device> devices) {
+        // find the first one that's active
+        Optional<Device> firstActive = devices.stream()
+                .filter(device -> !device.getIs_restricted())
+                .filter(Device::getIs_active)
+                .findFirst();
+
+        Optional<Device> firstPhone = devices.stream()
+                .filter(device -> device.getType().equals("smartphone"))
+                .findFirst();
+
+        return firstActive.or(() -> firstPhone);
+    }
+
+    private String formatSpotifyUriForTrack(String trackId) {
+        return String.format("spotify:track:%s", trackId);
     }
 }
--- src/main/java/com/mixtape/mixtapeapi/spotify/SpotifyService.java
+++ src/main/java/com/mixtape/mixtapeapi/spotify/SpotifyService.java
@@ -12,21 +12,30 @@ public interface SpotifyService {
     /**
      * Get a list of track infos from spotify. Note that this method DOES NOT let you throw checked exceptions. That is
      * on purpose. Catch your exceptions and throw a ResponseStatusException
-     *
      * @param ids Sequence of spotify ids to get
      * @return List of track infos
      * @throws ResponseStatusException An HTTP response exception that the user should see and make sense of
      */
-    List<TrackInfo> getTrackInfos(String... ids) throws ResponseStatusException;
+    List<TrackInfo> getTrackInfos(String ...ids) throws ResponseStatusException;
 
     /**
      * Get the duration of one or more tracks
-     *
      * @param ids Sequence of spotify ids to query
      * @return The sum duration of all the ids provided
      * @throws ResponseStatusException An HTTP response exception that the user should see and make sense of
      */
-    Duration getTracksDuration(String... ids) throws ResponseStatusException;
+    Duration getTracksDuration(String ...ids) throws ResponseStatusException;
+
+    void enqueueSongs(String token, List<String> ids) throws ResponseStatusException;
+
+    /**
+     * Check if a user follows any of the users given by their IDs.
+     * @param userProviderToken Provider token of the user making the request
+     * @param spotifyUserIDs List of spotify user IDs to check
+     * @return A list of spotify user ids that the user follows
+     * @throws ResponseStatusException An HTTP response exception if there is an error
+     */
+    List<String> checkFollowsAnyUsers(String userProviderToken, List<String> spotifyUserIDs) throws ResponseStatusException;
 
     List<TrackSimplified> getRecentlyListenedToTracks(String providerToken);
 }
